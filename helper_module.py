8029da5f9d880fb7b303ce9dd058776ac60813f3
Borhan <bmoyassari@gmail.com>
2024-08-17 18:54:03 +0330

    added json_io class with validation and append

--- helper_module.py
+++ helper_module.py
@@ -1,5 +1,8 @@
 import ipaddress
 import threading
+import os
+import json
+
 
 def ip_validator(input_str):
     try:
@@ -16,3 +19,22 @@ def threaded(fn):
         return _thread
 
     return wrapper
+
+
+class json_io:
+    @staticmethod
+    def append_to_json(dict_object, file_name):
+        if type(dict_object) == dict:
+            with open(file_name) as jsoni:
+                listobj = json.load(jsoni)
+                listobj.append(dict_object)
+
+            with open(file_name, 'w') as jsonw:
+                json.dump(listobj, jsonw, indent=4, separators=(',', ': '))
+
+    @staticmethod
+    def check_file_exists(file_name):
+        if os.path.isfile(file_name) is False:
+            f = open(file_name, 'w')
+            f.close()
+
