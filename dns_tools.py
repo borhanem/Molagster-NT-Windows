d83929b2fb3cb2621ec1c38b3a3d3fc5d2825ebb
Borhan <bmoyassari@gmail.com>
2024-08-17 17:50:24 +0330

    added dns_tools.py with empty class

---
+++ dns_tools.py
@@ -0,0 +1,14 @@
+import os
+
+#TODO: create dns servers.json and load dns servers from json file
+
+class dns_tools:
+
+    @staticmethod
+    def dns_query():
+        # query the input url using scapy
+        # use option for custom dns server
+        pass
+
+
+
