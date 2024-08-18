aae1727574a72f4b90784073f5a75231c4186f52
Borhan <bmoyassari@gmail.com>
2024-08-18 14:49:42 +0200

    Added set_dns and dns_off to dns_tools in dns_tools.py further examination is needed to decide if all methods should be static or not

--- dns_tools.py
+++ dns_tools.py
@@ -1,6 +1,9 @@
 import os
+import platform
+from pyuac import main_requires_admin
 
-#TODO: create dns servers.json and load dns servers from json file
+
+# TODO: create dns servers.json and load dns servers from json file
 
 class dns_tools:
 
@@ -10,5 +13,28 @@ class dns_tools:
         # use option for custom dns server
         pass
 
+    @staticmethod
+    @main_requires_admin
+    def set_dns(dns_dict_object, interface):
+        if platform.system() == "Windows":
+            os.system("netsh interface ip set dns \"" + interface + "\" static " + dns_dict_object["primary"])
+            if dns_dict_object["secondary"] != "null":
+                os.system(
+                    "netsh interface ip add dns \"" + interface + "\" " + dns_dict_object["secondary"] + " INDEX=2")
+        else:
+            raise Exception("Operating system not supported")
+
+    @staticmethod
+    @main_requires_admin
+    def disable_dns(interface):
+        if platform.system() == "Windows":
+            os.system("netsh interface ip set dns \"" + interface + "\" source=dhcp")
+        else:
+            raise Exception("Operating system not supported")
 
 
+class dns_storage_handling:
+
+    @staticmethod
+    def add_to_storage(dns_dict):
+        pass
