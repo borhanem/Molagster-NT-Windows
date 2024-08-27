d8bdd90e73c0a19a93c16cb787244271fd34fceb
Borhan <bmoyassari@gmail.com>
2024-08-27 15:02:03 +0330

    Fixed a bug with set_dns and added showint and dns_query skeleton

--- dns_tools.py
+++ dns_tools.py
@@ -1,26 +1,37 @@
 import os
 import platform
+import sys
+
+import helper_module as helper
 from pyuac import main_requires_admin
+from scapy.all import *
+import socket
+import subprocess
 
 
+# TODO : fix admin problems
 # TODO: create dns servers.json and load dns servers from json file
-
+# TODO: implement dns_query & showinterfaces
 class dns_tools:
-
     @staticmethod
     def dns_query():
-        # query the input url using scapy
-        # use option for custom dns server
+        pass
+
+    @staticmethod
+    @main_requires_admin
+    def showinterfaces():
         pass
 
     @staticmethod
     @main_requires_admin
     def set_dns(dns_dict_object, interface):
-        if platform.system() == "Windows":
-            os.system("netsh interface ip set dns \"" + interface + "\" static " + dns_dict_object["primary"])
+        if sys.platform == "win32":
+            command = "netsh interface ip set dns \"" + interface + "\" static " + dns_dict_object["primary"]
+            os.system(command)
             if dns_dict_object["secondary"] != "null":
-                os.system(
-                    "netsh interface ip add dns \"" + interface + "\" " + dns_dict_object["secondary"] + " INDEX=2")
+                secondary_command = "netsh interface ip add dns \"" + interface + "\" " + dns_dict_object[
+                    "secondary"] + " INDEX=2"
+                os.system(secondary_command)
         else:
             raise Exception("Operating system not supported")
 
