cb3ab9e031d844ac7f09e49e957de4c8832bc371
Borhan <bmoyassari@gmail.com>
2024-08-27 13:28:30 +0330

    Added network arp scan and display arp results to network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,1 +1,29 @@
+import scapy.all as scapy
 
+
+class Arp_Scan:
+
+    @staticmethod
+    def scan(ip):  # change to scan subnet
+        arp_req_frame = scapy.ARP(pdst=ip)
+
+        broadcast_ether_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+        broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
+
+        answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout=1, verbose=False)[0]
+        # print(answered_list)
+        # print(150 * '_')
+        # print(type(answered_list))
+        result = []
+        for i in range(0, len(answered_list)):
+            # print(answered_list[i])
+            client_dict = {"ip": answered_list[i][1].psrc, "mac": answered_list[i][1].hwsrc}
+            result.append(client_dict)
+
+        Arp_Scan.display_result(result)
+
+    @staticmethod
+    def display_result(result):
+        for element in result:
+            print(f"IP: {element['ip']} MAC: {element['mac']}")
